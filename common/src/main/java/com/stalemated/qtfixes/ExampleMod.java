cc55e2d3c25ce305085d375e92667c0fb2b7b488
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-08-14 02:08:21 -0600

    Initial commit

---
+++ common/src/main/java/com/stalemated/qtfixes/ExampleMod.java
@@ -0,0 +1,9 @@
+package com.stalemated.qtfixes;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "quests_teams_fixes";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
